d630dc0ec33bcefd53b238a316a17aa836725d94
Renato <114404480+normalmant@users.noreply.github.com>
2026-08-13 19:28:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,27 +9,82 @@ const fs = require('fs') // importa leitura e escrita de arquivos.
 
 
 
-
-
-
-
 app.get("/aulas/:Dia",(req,res)=>{
+    const Dia=req.params.Dia
+
+    try{
+         const aula= JSON.parse(fs.readFileSync("aulas.json","utf8"))
+
+         const aula_dia= aula.filter((aula)=>aula.Dia.toLowerCase()===Dia.toLowerCase())
+
+         if(aula_dia.length===0){
+            res.status(400).json({resposta:"Não existe aulas existentes neste dia"})
+         }
+
+         res.status(200).json({resposta: aula_dia})
+
+    }catch(erro){
+    res.status(500).json({erro:"Erro interno do servidor"})
+    }
+})
+
+
+
+
+
+app.put("/aulas/:id",(req,res)=>{
+    const id= Number(req.params.id)
+
+
+    try{
+        const aulas= JSON.parse(fs.readFileSync("aulas.json","utf8"))
+
+        const indiceAula=aulas.findIndex((aula)=>aula.id===id)
+
+        if(indiceAula===-1){
+            res.status(404).json({erro:"Aula não foi encontrada"})
+        }
+        
+        aulas[indiceAula]={
+            ...req.body,
+            id:id
+        }
+
+        console.log(indiceAula)
+        res.status(200).json({resposta:"Aula foi Alterada"})
+
+        fs.writeFileSync("aulas.json",JSON.stringify(aulas),"utf8")
+
+    }catch(erro){
+        res.status(500).json({erro:"Erro interno do servidor"})
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+//Visualizar todas as aulas
+app.get("/aulas",(req,res)=>{
 
     const Dia= req.params.Dia
     
         try{
-
             const aula= JSON.parse(fs.readFileSync("aulas.json","utf8"))
-            res.status(200).json({resposta:aula})
-    
-            if(indiceAula==-1){
-                
-                return res.status(404).json({erro: "Aula não existe no Banco de Dados"})
+            
+            if(aula===0){
+               res.status(400).json({resposta:"Não existe nada para ser mostrado"})
+            }else{
+             res.status(200).json({resposta:aula})
             }
-    
-    
-            fs.writeFileSync("aulas.json",JSON.stringify(bd),"utf8")
-            res.status(200).json({resposta:"Aula apagado"})
     
         }catch(erro){
             res.status(500).json({erro:"Deu errado"})
@@ -45,22 +100,35 @@ app.get("/aulas/:Dia",(req,res)=>{
 
 
 
-
 app.post("/aulas",(req,res)=>{
 
     const aulas=req.body
 
     try{
-       const segunda= JSON.parse(fs.readFileSync("aulas.json","utf8") )
+       const bd= JSON.parse(fs.readFileSync("aulas.json","utf8") )
 
-        segunda.push(aulas)
 
-        fs.writeFileSync("aulas.json",JSON.stringify(segunda),"utf8")
+       //Adicionar ID automaticamente
+       const contador=JSON.parse(fs.readFileSync("id.json","utf8"))
 
-        res.status(200).json({resposta:"Famoso Adicionado"})
+       contador.id=contador.id +1
+       fs.writeFileSync("id.json",JSON.stringify(contador),"utf8")
+
+
+
+        aulas.id=contador.id
+
+
+
+
+        bd.push(aulas)
+        fs.writeFileSync("aulas.json",JSON.stringify(bd),"utf8")
+
+        res.status(200).json({resposta:"Aula adicionada"})
     }catch{
         res.status(500).json({erro:"Falha ao cadastrar"})
     }
+    
 })
 
 
@@ -69,4 +137,8 @@ app.post("/aulas",(req,res)=>{
 // Execução da API:
 app.listen(port, ()=>{
     console.log("API rodando na porta " + port)
+
+        const contador=JSON.parse(fs.readFileSync("id.json","utf8"))
+        contador.id=contador.id 
+        console.log(contador)
 })
